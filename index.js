b1da155952f02f1b50b61a1342fe781cf63dd52b
Animesh478 <animeshmohanty.am@gmail.com>
2025-08-23 01:17:25 +0530

    table created

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Connection has been created");
+
+  const createQuery = `CREATE TABLE IF NOT EXISTS Students(
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20),
+  email VARCHAR(50)
+  );`;
+
+  connection.execute(createQuery, (err) => {
+    if (err) {
+      console.log(err);
+      connection.end();
+      return;
+    }
+    console.log("Table is created");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
